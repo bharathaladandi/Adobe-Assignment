6f4df6cd6de8489db4d90c8574649b7835664095
bharathaladandi <bharathaladandi@gmail.com>
2023-04-08 18:32:40 +0530

    user schema created

---
+++ Backend/models/User.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+
+    email:{ type:String, required: true },
+    password:{ type:String, required: true },
+    name:{ type:String, required: true },
+    age:{ type:String, required: true },
+
+},
+{
+    versionKey: false,
+    timestamps: true,
+})
+
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
